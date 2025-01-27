2128d0757c601a9dd0adba92bc7b1f6a8d6a07af
Danilo de Paula Vieira <72302140+danzbz@users.noreply.github.com>
2025-01-26 22:44:04 -0300

    Update supabaseClient.ts

--- app/src/supabaseClient.ts
+++ app/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Substitua pelos valores da sua URL e chave anônima do Supabase
+// Substitua pelos valores da sua URL e chave anônima do Supabase 
 const supabaseUrl = 'https://gphtgbuxmwijwchzyjno.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwaHRnYnV4bXdpandjaHp5am5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2NDg4OTksImV4cCI6MjA0NTIyNDg5OX0.MnWkUgqIEScxFFydBpO6OXscuBFaJBoIKdLv6yoecrs'
 
